4d8c3c00dc2a70693375c9c5ae5c79e56a26284d
Sahil Panwar <sahil.panwar@gmail.com>
2013-06-22 17:17:12 -0400

    Added GameObject class

---
+++ BattleCrittersMetro/BattleCrittersMetro/GameObject.cs
@@ -0,0 +1,25 @@
+﻿using SharpDX;
+using SharpDX.Direct3D11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleCrittersMetro
+{
+    public class GameObject
+    {
+        public Vector2 position;
+        public Texture2D texture;
+
+        public GameObject()
+        {
+
+        }
+
+
+
+
+    }
+}
