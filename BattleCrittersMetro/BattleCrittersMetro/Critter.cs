00bcdbbf8e0e44389688727baf2b18f9ab81383c
Nicole Flokos <nicole.flokos@gmail.com>
2013-06-22 17:35:53 -0400

    Created Critter and Trainer class

---
+++ BattleCrittersMetro/BattleCrittersMetro/Critter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleCrittersMetro
+{
+    public class Critter : GameObject
+    {
+        public Element[] Element;
+        public int Health;
+        public int Mana;
+        public int Attack;
+        public int Defense;
+        public int Speed;
+        public Attack[] Attacks;
+        public Guid ID;
+    }
+}
---
+++ BattleCrittersMetro/BattleCrittersMetro/Trainer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleCrittersMetro
+{
+    public class Trainer : GameObject
+    {
+        public string Name;
+        public char Gender;
+        public int Money;
+        public Guid ID;
+        public Vector2 Postion;
+        public string Location;
+        public Critter[] Critters;
+
+    }
+}
